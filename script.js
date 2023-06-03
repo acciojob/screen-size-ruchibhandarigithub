08961e59a503c57fb888a880383238f73b4d4d59
Ruchi Bhandari <98799210+ruchibhandarigithub@users.noreply.github.com>
2023-06-03 20:03:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+function getWindowSize() {
+  const width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+  const height = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+  
+  const sizeInfoDiv = document.getElementById('sizeInfo');
+  const sizeText = `Width: ${width}px<br>Height: ${height}px`;
+  
+  sizeInfoDiv.innerHTML = `<h1>${sizeText}</h1>`;
+}
+
+window.addEventListener('resize', getWindowSize);
+I
